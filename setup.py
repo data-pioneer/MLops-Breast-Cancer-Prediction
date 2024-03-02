dd8c68913be077d3c250857cd590162fb2184ab9
Jaspreet Kaur <jasika.lpu@gmail.com>
2024-03-02 12:37:41 +0530

    requirement and setup files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cancer_prediction",
+    version="0.0.0",
+    author="Jaspreet",
+    author_email="jasika.lpu@gmail.com",
+    packages=find_packages()
+)
